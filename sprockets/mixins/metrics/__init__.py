8528beef5d10355af07f641b4987df3cd64a7b0f
Dave Shawley <daves@aweber.com>
2016-02-01 14:48:46 -0500

    Make it safe to import __version__.

--- sprockets/mixins/metrics/__init__.py
+++ sprockets/mixins/metrics/__init__.py
@@ -1,5 +1,12 @@
-from .influxdb import InfluxDBMixin
-from .statsd import StatsdMixin
+try:
+    from .influxdb import InfluxDBMixin
+    from .statsd import StatsdMixin
+except ImportError as error:
+    def InfluxDBMixin(*args, **kwargs):
+        raise error
+
+    def StatsdMixin(*args, **kwargs):
+        raise error
 
 version_info = (1, 0, 0)
 __version__ = '.'.join(str(v) for v in version_info)
