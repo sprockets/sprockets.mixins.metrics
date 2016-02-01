05c013142d51181ae81107f19e0a813b79accf5e
Dave Shawley <daves@aweber.com>
2016-02-01 14:49:19 -0500

    Export version_info and __version__.

--- sprockets/mixins/metrics/__init__.py
+++ sprockets/mixins/metrics/__init__.py
@@ -10,4 +10,4 @@ except ImportError as error:
 
 version_info = (1, 0, 0)
 __version__ = '.'.join(str(v) for v in version_info)
-__all__ = ['InfluxDBMixin', 'StatsdMixin']
+__all__ = ['__version__', 'version_info', 'InfluxDBMixin', 'StatsdMixin']
